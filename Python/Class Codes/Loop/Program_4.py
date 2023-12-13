9c95777b153351d787d44514f9095952ff301816
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-12-13 10:09:03 +0530

    Loop

---
+++ Python/Class Codes/Loop/Program_4.py
@@ -0,0 +1,11 @@
+PlayerList = ["Rohit", "Ishan", "Surya", "Tilak", "Hardik"]
+
+for i in PlayerList :
+    print(i)
+
+for i in PlayerList :
+    if "Rohit" == i :
+        print("Captain")
+        break
+else :
+    print("Captain not present")
---
+++ Python/Class Codes/Loop/Program_5.py
@@ -0,0 +1,5 @@
+
+for i in range(1, 10) :
+    if i == 5:
+        pass 
+    print(i)
